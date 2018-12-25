f4e116968b1de5072db462b86de6c01f67caae90
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2018-12-25 23:28:47 +0300

    help en+ru (html) done

--- app/src/main/java/com/pierbezuhoff/dodeca/HelpActivity.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/HelpActivity.kt
@@ -8,8 +8,8 @@ class HelpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        title = "Help"
+        title = getString(R.string.help_title)
         setContentView(R.layout.activity_help)
-        help.loadUrl("file:///android_asset/help.html")
+        help.loadUrl("file:///android_res/raw/help.html")
     }
 }
