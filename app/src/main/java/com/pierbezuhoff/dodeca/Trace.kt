1e60cd7faf2fe700ae67482f31c4143a01ed8d62
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2018-12-26 21:15:47 +0300

    exp: +-

--- app/src/main/java/com/pierbezuhoff/dodeca/Trace.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/Trace.kt
@@ -10,8 +10,8 @@ class Trace(val paint: Paint) {
     var initialized = false
     lateinit var bitmap: Bitmap
     lateinit var canvas: Canvas
-//    val translation = Matrix()
-    val motion = Matrix()
+    val translation = Matrix()
+//    val motion = Matrix()
     // `bitmap` top-left corner - screen top-left corner
     var dx: Float = 0f
     var dy: Float = 0f
@@ -23,9 +23,9 @@ class Trace(val paint: Paint) {
         dx = (1f - factor) * width / 2
         dy = (1f - factor) * height / 2
         canvas = Canvas(bitmap)
-//        translation.reset()
-//        translation.preTranslate(dx, dy)
-        motion.reset()
+        translation.reset()
+        translation.preTranslate(dx, dy)
+//        motion.reset()
         initialized = true
     }
 
