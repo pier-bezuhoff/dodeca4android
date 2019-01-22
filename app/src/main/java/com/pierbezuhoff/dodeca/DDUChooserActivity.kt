9f40903ef9d069804b370ae67ebbfc6d6e681003
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2019-01-23 02:25:35 +0300

    rename & chooser context menu almost done

--- app/src/main/java/com/pierbezuhoff/dodeca/DDUChooserActivity.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/DDUChooserActivity.kt
@@ -8,8 +8,10 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -21,20 +23,25 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_dduchooser.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.cancelButton
+import org.jetbrains.anko.customView
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 import java.io.File
 
 typealias Filename = String
 
+// TODO: action bar: import file/folder, export folder
 // TODO: store in sharedPreferences last dir
-// TODO: preview for ddu
 // TODO: go to parent folder
 // MAYBE: link to external folder
 class DDUChooserActivity : AppCompatActivity() {
     private lateinit var dduDir: File
     private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewAdapter: DDUAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +51,7 @@ class DDUChooserActivity : AppCompatActivity() {
             dduDir = File(it)
         }
         setContentView(R.layout.activity_dduchooser)
-        viewAdapter = DDUAdapter(dduDir, ::onChoose)
+        viewAdapter = DDUAdapter(this, dduDir, ::onChoose)
         recyclerView = ddu_recycler_view.apply {
             adapter = viewAdapter
             // colors and thickness are set from styles.xml
@@ -62,6 +69,60 @@ class DDUChooserActivity : AppCompatActivity() {
         }
         setResult(Activity.RESULT_OK, data)
         finish()
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        super.onContextItemSelected(item)
+        var result = true
+        viewAdapter.contextMenuCreatorPosition?.let { position ->
+            val file = viewAdapter.files[position]
+            val filename: Filename = file.nameWithoutExtension
+            when (item?.itemId) {
+                R.id.ddu_rename -> {
+                    var editText: EditText? = null
+                    // BUG: SpannableStringBuilder: SPAN_EXCLUSIVE_EXCLUSIVE spans cannot have a zero length
+                    alert("Rename ddu file $filename") {
+                        customView {
+                            editText = editText(filename)
+                        }
+                        positiveButton("Rename") {
+                            editText?.text?.toString()?.let { newFilename ->
+                                toast("rename $file to $newFilename")
+                                val newFile = File("${file.parentFile.absolutePath}/$newFilename.ddu")
+                                val success = file.renameTo(newFile)
+                                Log.i(TAG, "$file -> $newFile: $success")
+                                if (success) {
+                                    // BUG: should avoid calc of preview
+                                    with(DB.dduFileDao()) {
+                                        update(findByFilename(file.name).apply { this.filename = newFilename })
+                                    }
+                                    viewAdapter.files[position] = newFile
+                                    viewAdapter.notifyDataSetChanged()
+                                } else {
+                                    Log.w(TAG, "failed to rename $file to $newFile")
+                                }
+                            }
+                        }
+                        cancelButton { }
+                    }.show()
+                }
+                R.id.ddu_delete -> {
+                    toast("delete $file")
+                    with(DB.dduFileDao()) {
+                        delete(findByFilename(file.name))
+                    }
+                    file.delete()
+                    viewAdapter.files.removeAt(position)
+                    viewAdapter.notifyDataSetChanged()
+                }
+                R.id.ddu_restore -> {
+                    // MainActivity.extract it
+                    toast("restore $file")
+                }
+                else -> result = false
+            }
+        }
+        return result
     }
 
     companion object {
@@ -105,14 +166,16 @@ class AutofitGridRecyclerView @JvmOverloads constructor(
 }
 
 class DDUAdapter(
+    val activity: AppCompatActivity,
     private var dir: File,
     private val onChoose: (File) -> Unit
 ) : RecyclerView.Adapter<DDUAdapter.DDUViewHolder>() {
     class DDUViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-    private var files: Array<File> = dir.listFiles()
+    var contextMenuCreatorPosition: Int? = null // track VH to act on it
+    var files: ArrayList<File> = ArrayList(dir.listFiles()
         .filter { it.extension.toLowerCase() == "ddu" }
-        .toTypedArray()
+    )
     private val dduFileDao: DDUFileDao by lazy { DB.dduFileDao() }
     private val previews: MutableMap<Filename, Bitmap?> = mutableMapOf()
     private val building: MutableSet<Filename> = mutableSetOf()
@@ -138,6 +201,11 @@ class DDUAdapter(
             val filename = file.name.removeSuffix(".ddu").removeSuffix(".DDU")
             findViewById<TextView>(R.id.ddu_entry).text = filename
             setOnClickListener { onItemClick(file) }
+            activity.registerForContextMenu(this)
+            setOnCreateContextMenuListener { menu, _, _ ->
+                contextMenuCreatorPosition = position
+                activity.menuInflater.inflate(R.menu.ddu_chooser_context_menu, menu)
+            }
         }
         holder.setIsRecyclable(false)
         val preview: ImageView = holder.view.findViewById(R.id.ddu_preview)
@@ -178,7 +246,7 @@ class DDUAdapter(
     private fun onItemClick(item: File) {
         if (item.isDirectory) {
             dir = item
-            files = dir.listFiles()
+            files = ArrayList(dir.listFiles().toList())
             notifyDataSetChanged()
         } else {
             onChoose(item)
