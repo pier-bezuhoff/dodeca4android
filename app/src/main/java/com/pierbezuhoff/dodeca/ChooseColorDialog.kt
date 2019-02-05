15e6db225413a9429bcd81965887c14e007173e4
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2019-02-05 19:31:50 +0300

    added border/fill layer:

--- app/src/main/java/com/pierbezuhoff/dodeca/ChooseColorDialog.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/ChooseColorDialog.kt
@@ -1,26 +1,26 @@
 package com.pierbezuhoff.dodeca
 
 import android.app.Dialog
-import android.graphics.drawable.ColorDrawable
+import android.graphics.PorterDuff
+import android.graphics.drawable.LayerDrawable
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.choose_color_row.view.*
-import kotlinx.android.synthetic.main.color_circle_row.view.*
 
 class ChooseColorDialog(val activity: MainActivity, dodecaView: DodecaView)  {
-    private val data: Array<ColorGroup> by lazy {
+    private val data: Array<CircleFigure> by lazy {
         dodecaView.circleGroup.figures
-            .filter { it.show }
-            .groupBy { it.color }
-            .toSortedMap()
-            .map { (color, circles) -> ColorGroup(color, circles) }
+            .filter { it.show } // maybe: also show invisible circles in end + options.showAllCircles
+            .sortedBy { it.color }
             .toTypedArray()
     }
-    var listener: ChooseColorListener = activity
+    private var listener: ChooseColorListener = activity
     interface ChooseColorListener {
         fun onChooseColorClosed()
     }
@@ -31,7 +31,7 @@ class ChooseColorDialog(val activity: MainActivity, dodecaView: DodecaView)  {
         val layout = inflater.inflate(R.layout.choose_color_dialog, null)
         builder.setView(layout)
         val manager = LinearLayoutManager(activity)
-        val colorGroupAdapter = ColorGroupAdapter(data)
+        val colorGroupAdapter = CirclesAdapter(data)
         layout.findViewById<RecyclerView>(R.id.color_groups)!!.apply {
             layoutManager = manager
             adapter = colorGroupAdapter
@@ -51,43 +51,38 @@ class ChooseColorDialog(val activity: MainActivity, dodecaView: DodecaView)  {
     }
 }
 
-class ColorGroupAdapter(
-    private val colorGroups: Array<ColorGroup>
-) : RecyclerView.Adapter<ColorGroupAdapter.ViewHolder>() {
-    class ViewHolder(val row: LinearLayout) : RecyclerView.ViewHolder(row)
+class CirclesAdapter(
+    private val circles: Array<CircleFigure>
+) : RecyclerView.Adapter<CirclesAdapter.ViewHolder>() {
+    class ViewHolder(val row: View) : RecyclerView.ViewHolder(row)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val row = LayoutInflater.from(parent.context)
-            .inflate(R.layout.choose_color_row, parent, false) as LinearLayout
-        return ViewHolder(row)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val colorGroup = colorGroups[position]
-        holder.row.group_color.setImageDrawable(ColorDrawable(colorGroup.color))
-        // setup circle group
-    }
-
-    override fun getItemCount(): Int = colorGroups.size
-}
-
-data class ColorGroup(val color: Int, val circles: List<CircleFigure>)
-
-class ColorCircleAdapter(
-    private val circles: List<CircleFigure>
-) : RecyclerView.Adapter<ColorCircleAdapter.ViewHolder>() {
-    class ViewHolder(val row: LinearLayout) : RecyclerView.ViewHolder(row)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val row = LayoutInflater.from(parent.context)
-            .inflate(R.layout.color_circle_row, parent, false) as LinearLayout
+            .inflate(R.layout.choose_color_row, parent, false)
         return ViewHolder(row)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val circle = circles[position]
-        holder.row.circle_color.setImageDrawable(ColorDrawable(circle.color))
-        // setup circle
+        with(holder.row) {
+            this.circle_name.text = "$position"
+            val circleImage =
+                ResourcesCompat.getDrawable(resources, R.drawable.circle_image, null) as LayerDrawable
+            circleImage.mutate()
+            val border = circleImage.getDrawable(0)
+            val inner = circleImage.getDrawable(1)
+            if (circle.fill) {
+                inner.setColorFilter(circle.color, PorterDuff.Mode.SRC_ATOP)
+                border.alpha = 0
+            } else {
+                inner.alpha = 0
+                border.setColorFilter(circle.borderColor ?: circle.color, PorterDuff.Mode.SRC_ATOP)
+            }
+            this.circle_image.setImageDrawable(circleImage)
+            setOnClickListener {
+                Log.i("ChooseColorDialog", "pressed circle $position")
+            }
+        }
     }
 
     override fun getItemCount(): Int = circles.size
