b74743b5f0dba92e42999d7d20256a53273189e9
Ivan Nikitin <pierbezuhoff2016@gmail.com>
2019-06-27 15:49:38 +0300

    adding DduFileService

--- app/src/main/java/com/pierbezuhoff/dodeca/ui/meta/DodecaAndroidViewModel.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/ui/meta/DodecaAndroidViewModel.kt
@@ -4,11 +4,11 @@ import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import com.pierbezuhoff.dodeca.models.DduFileRepository
+import com.pierbezuhoff.dodeca.models.DduFileService
 
 abstract class DodecaAndroidViewModel(application: Application) : AndroidViewModel(application) {
     protected val context: Context
         get() = getApplication<Application>().applicationContext
-    protected val dduFileRepository: DduFileRepository by lazy {
-        DduFileRepository.get(context)
-    }
+    protected val dduFileRepository: DduFileRepository = DduFileRepository.get(context)
+    protected val dduFileService: DduFileService = DduFileService(context)
 }
--- app/src/main/java/com/pierbezuhoff/dodeca/ui/meta/DodecaAndroidViewModelWithOptionsManager.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/ui/meta/DodecaAndroidViewModelWithOptionsManager.kt
@@ -1,9 +1,16 @@
 package com.pierbezuhoff.dodeca.ui.meta
 
 import android.app.Application
+import com.pierbezuhoff.dodeca.data.options
 import com.pierbezuhoff.dodeca.models.OptionsManager
+import com.pierbezuhoff.dodeca.utils.dduDir
+import com.pierbezuhoff.dodeca.utils.div
+import java.io.File
 
 abstract class DodecaAndroidViewModelWithOptionsManager(
     application: Application,
     protected val optionsManager: OptionsManager
-) : DodecaAndroidViewModel(application)
+) : DodecaAndroidViewModel(application) {
+    val dir: File
+        get() = (context.dduDir/optionsManager.fetched(options.recentDdu)).parentFile ?: context.dduDir
+}
