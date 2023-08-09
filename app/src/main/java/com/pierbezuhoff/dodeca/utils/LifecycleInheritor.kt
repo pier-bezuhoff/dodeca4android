98c74624a54006af1c0496a8714efe239700a81b
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2023-08-09 14:01:53 +0300

    downgraded deps and ddu are playing now, editor still broken tho

--- app/src/main/java/com/pierbezuhoff/dodeca/utils/LifecycleInheritor.kt
+++ app/src/main/java/com/pierbezuhoff/dodeca/utils/LifecycleInheritor.kt
@@ -26,7 +26,9 @@ class LifecycleInheritance : LifecycleInheritor {
     override val lifecycleInherited: Boolean get() = inherited
     private var _lifecycle: Lifecycle = LifecycleRegistry(this)
 
-    override val lifecycle: Lifecycle = _lifecycle
+//    override val lifecycle: Lifecycle = _lifecycle
+    override fun getLifecycle(): Lifecycle =
+        _lifecycle
 
     override fun inheritLifecycleOf(lifecycleOwner: LifecycleOwner) {
         require(!inherited)
